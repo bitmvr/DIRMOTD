fcae569adbc71b7f841e247f7b937a5ddff62ba0
Jesse Riddle <jriddle@salesforce.com>
2017-11-20 16:02:52 -0500

    Added dirmotd.sh

---
+++ dirmotd.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+cwdStore='/tmp/.cwd'
+
+function get_working_dir() {
+	pwd
+}
+
+function read_cwdStore() {
+	cat "${cwdStore}"
+}
+
+function create_cwdStore() {
+	touch "${cwdStore}"
+}
+
+function update_cwdStore() {
+	echo "${1}" > "${cwdStore}"
+}
+
+function previous_dir() {
+	if [ -f "${cwdStore}" ]; then
+		read_cwdStore
+	else
+		create_cwdStore
+		read_cwdStore
+	fi
+}
+
+function read_testFile() {
+  if [ -f 'test_file' ]; then
+    cat test_file
+  fi
+}
+
+if [ "$(get_working_dir)" == "$(previous_dir)" ]; then
+	exit 0
+else
+	update_cwdStore "$(get_working_dir)"
+	read_testFile
+fi
